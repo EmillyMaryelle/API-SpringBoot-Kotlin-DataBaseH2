aa86467e943a4ce1b3c637b2233c0c0fe39e1125
EmillyMaryelle <emillymaryelle@gmail.com>
2023-05-14 19:10:36 -0300

    try

--- src/main/kotlin/controller.kts
+++ src/main/kotlin/controller.kts
@@ -2,12 +2,12 @@
 @RequestMapping("/main")
 class mainController(val MainRepository: MainRepository) {
 
-    @PostMapping ("/api/generate")
+    @PostMapping("/api/generate")
     fun generatemain(@RequestBody main: main): main {
         return MainRepository.save(main)
     }
 
-    @GetMapping ("/api/get")
+    @GetMapping("/api/get")
     fun getAllmains(): List<main> {
         return MainRepository.findAll()
     }
