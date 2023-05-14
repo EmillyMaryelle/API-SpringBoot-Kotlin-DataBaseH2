0b5751a5ad34bb3d6909be7adca82a323829c692
EmillyMaryelle <emillymaryelle@gmail.com>
2023-05-14 19:08:52 -0300

    aplicacao end point

--- src/main/kotlin/controller.kts
+++ src/main/kotlin/controller.kts
@@ -2,12 +2,12 @@
 @RequestMapping("/main")
 class mainController(val MainRepository: MainRepository) {
 
-    @PostMapping
+    @PostMapping ("/api/generate")
     fun generatemain(@RequestBody main: main): main {
         return MainRepository.save(main)
     }
 
-    @GetMapping
+    @GetMapping ("/api/get")
     fun getAllmains(): List<main> {
         return MainRepository.findAll()
     }
